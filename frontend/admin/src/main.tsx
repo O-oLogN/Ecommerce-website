542e653cc60c280c1594c0eda81411cc2e0c2338
OLogN <vinhhiepbn2003.work@gmail.com>
2025-01-12 10:19:14 +0700

    feat: update

--- frontend/admin/src/main.tsx
+++ frontend/admin/src/main.tsx
@@ -1,14 +1,21 @@
 import { createRoot } from 'react-dom/client'
 import {LoginPage} from "./pages/Login";
 import {QueryClient, QueryClientProvider} from "react-query";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {LoginContextProvider} from "./pages/Login/hooks/LoginContext.tsx";
+import { HomePage } from './pages/HomePage/index.tsx';
 
 const App = () => {
     const queryClient = new QueryClient()
     return (
         <QueryClientProvider client={ queryClient }>
             <LoginContextProvider>
-                <LoginPage />
+                <Router>
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/home" element={<HomePage />} />
+                    </Routes>
+                </Router>
             </LoginContextProvider>
         </QueryClientProvider>
     )
---
+++ frontend/admin/src/pages/HomePage/index.tsx
@@ -0,0 +1,7 @@
+import {SideMenu} from '../../layouts/index.ts'
+
+export const HomePage = () => {
+    return (
+        <SideMenu />
+    )
+}
