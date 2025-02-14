ea1207fae50981d87abb38d0900340b4e572141f
OLogN <vinhhiepbn2003.work@gmail.com>
2025-02-14 14:18:19 +0700

    feat: Update UI --

---
+++ frontend/client/src/pages/CheckoutPage/components/Payment.tsx
@@ -0,0 +1,48 @@
+import {PaymentProps} from "pages/CheckoutPage/types"
+import React, {useEffect} from "react"
+import PaymentMethod from "pages/CheckoutPage/components/PaymentMethod.tsx"
+import Header from "pages/CheckoutPage/components/Header.tsx";
+
+const Payment: React.FC<PaymentProps> = (
+    {
+        paymentMethods,
+        setSelectedPaymentMethod,
+        status,
+        setStatus,
+    }) => {
+    const [selectedPaymentMethod, saveSelectedPaymentMethod] = React.useState<string>('')
+    const onGoToReviewButtonClick = () => {
+        if (selectedPaymentMethod === '') return
+        setStatus([status[0], status[1], true])
+    }
+
+    useEffect(() => {
+        if (status[0] && status[1] && status[2]) {
+            setSelectedPaymentMethod(selectedPaymentMethod)
+        }
+    }, [status])
+
+    return (
+        <div>
+            <Header
+                name='Payment'
+                sectionIndex={2}
+                status={ status }
+                setStatus={ setStatus }
+            />
+            { !status[2] && paymentMethods.map(paymentMethod =>
+                <PaymentMethod
+                    name={ paymentMethod.name }
+                    imageUrl={ paymentMethod.imageUrl }
+                    selectedPaymentMethod={ selectedPaymentMethod }
+                    setSelectedPaymentMethod={ saveSelectedPaymentMethod }
+                />
+            ) }
+            <button onClick={ onGoToReviewButtonClick } >
+                Go to Review
+            </button>
+        </div>
+    )
+}
+
+export default Payment
---
+++ frontend/client/src/pages/CheckoutPage/components/PaymentMethod.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {PaymentMethodProps} from "pages/CheckoutPage/types"
+
+const PaymentMethod: React.FC<PaymentMethodProps> = ({ name, imageUrl, selectedPaymentMethod, setSelectedPaymentMethod }) => {
+    return (
+        <div className={`flex items-center justify-between text-small-regular cursor-pointer py-4 border rounded-rounded px-8 mb-2 hover:shadow-borders-interactive-with-active border-ui-border-interactive ${ selectedPaymentMethod === name ? 'border-blue-700' : '' }`}
+             onClick={ () => setSelectedPaymentMethod(name) }
+        >
+            <span>{ name }</span>
+            <img src={ imageUrl } alt={ name } />
+        </div>
+    )
+}
+
+export default PaymentMethod
